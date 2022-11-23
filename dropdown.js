c3c80ce812a91a121258c73ecaaf1f7c59ddaa44
le-aslin <leia.aslin.gbg22@amerikanskagymnasiet.se>
2022-11-23 15:11:51 +0100

    fixed duo page

--- dropdown.js
+++ dropdown.js
@@ -17,3 +17,18 @@ window.onclick = function(event) {
     }
   }
 }
+function myFunction(one) {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn-one')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content-one");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
